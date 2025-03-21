4736fcd142138debe31307e783c2cc0f23203ba8
xoyn222 <kostuk186@gmail.com>
2025-03-21 22:46:45 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ const App = () => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [currentUser, setCurrentUser] = useState(null);
     const [loading, setLoading] = useState(true);
-    const navigate = useNavigate();
 
     useEffect(() => {
         const checkAuth = async () => {
